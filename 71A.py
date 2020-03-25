1630389284bc8392aa513df998f19287da1341aa
Hasanur <hasanseam37@gmail.com>
2020-03-25 22:39:33 +0100

    accepted

---
+++ 71A.py
@@ -0,0 +1,13 @@
+'''
+https://codeforces.com/problemset/problem/71/A
+Author: Arrow
+'''
+testCase = int(input())
+
+for i in range(testCase):
+    inputString = list(input())
+    strLength = len(inputString)
+    if strLength<=10:
+     print("".join(inputString))
+    else:
+     print("{}{}{}".format(inputString[0],strLength-2,inputString[strLength-1]))
