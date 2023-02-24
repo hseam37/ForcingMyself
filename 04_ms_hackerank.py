529e63b7ce1aaa306564272f87839f0cec75eece
Hasanur Jaman Seam <31930816+hasanseam@users.noreply.github.com>
2023-02-24 21:33:05 +0100

    Create 04_ms_hackerank.py

---
+++ 04_ms_hackerank.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    a=[]
+    # Write your code here
+    for i in range(len(queries)):
+        count=0
+        for j in range(len(strings)):
+            if(queries[i]==strings[j]):
+                count+=1
+        print(count)
+
+if __name__ == '__main__':
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
